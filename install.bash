1723b0d7e435e2c3181f9c4f3776255b5d229d01
Daniel Sirb <d4nny02@yahoo.com>
2025-10-29 23:37:41 +0200

    integrated initial install of immich

--- install.bash
+++ install.bash
@@ -3,9 +3,9 @@
 #Update OS
 dnf update -y
 
-# Install Git
+# Install Git & Tools
 dnf install git -y
-
+dnf install wget -y
 
 dnf -y install dnf-utils
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -21,3 +21,30 @@ systemctl enable docker
 docker --version
 docker compose version
 
+mkdir -p /home/docker-compose/immich-app
+cd /home/docker-compose/immich-app
+
+
+wget -O docker-compose.yml https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
+
+# Get important env file that has to be copied an migrated in case it is needed.
+wget -O .env https://github.com/immich-app/immich/releases/latest/download/example.env
+
+# Set a secure password for Posgress DB needed for Immich 
+# This field will be later changed with a secret as phrase
+sed -i 's/^DB_PASSWORD=.*$/DB_PASSWORD=jjZ77fY8c3h!M8wvYRT4H@Q/' .env
+
+# Set the timezone
+sed -i -e '/^#[[:space:]]*TZ=/ c\TZ=Europe/Bucharest' -e '/^TZ=/ c\TZ=Europe/Bucharest' .env
+
+# Set immich libraries location
+mkdir -p /data/docker-containers/immich/library
+sed -i 's|^UPLOAD_LOCATION=.*$|UPLOAD_LOCATION=/data/docker-containers/immich/library|' .env
+
+# Create Immich container
+docker compose up -d
+
+## Check the container
+# http://<machine-ip-address>:2283
+
+s

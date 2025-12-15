09ca411306a0998c6492d3e9943b677e134f3af3
Daniel Sirb <d4nny02@yahoo.com>
2025-12-15 13:42:28 +0200

    added paperless-ng and created variable for deailt directories

--- install.bash
+++ install.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# General Variables
+DOCKER_DATA=/home/data/docker-containers
+DOCKER_COMPOSE_DIR=/home/docker-compose
 #Update OS
 dnf update -y
 
@@ -21,8 +23,8 @@ systemctl enable docker
 docker --version
 docker compose version
 
-mkdir -p /home/docker-compose/immich-app
-cd /home/docker-compose/immich-app
+mkdir -p $DOCKER_COMPOSE_DIR/immich-app
+cd $DOCKER_COMPOSE_DIR/immich-app
 
 
 wget -O docker-compose.yml https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
@@ -38,8 +40,8 @@ sed -i 's/^DB_PASSWORD=.*$/DB_PASSWORD=jjZ77fY8c3h!M8wvYRT4H@Q/' .env
 sed -i -e '/^#[[:space:]]*TZ=/ c\TZ=Europe/Bucharest' -e '/^TZ=/ c\TZ=Europe/Bucharest' .env
 
 # Set immich libraries location
-mkdir -p /home/data/docker-containers/immich/library
-sed -i 's|^UPLOAD_LOCATION=.*$|UPLOAD_LOCATION=/home/data/docker-containers/immich/library|' .env
+mkdir -p $DOCKER_DATA/immich/library
+sed -i 's|^UPLOAD_LOCATION=.*$|UPLOAD_LOCATION=$DOCKER_DATA/immich/library|' .env
 
 # Create Immich container
 docker compose up -d
@@ -48,7 +50,7 @@ docker compose up -d
 # http://<machine-ip-address>:2283
 
 # Plex Configuration
-PLEX_HOME_DIR=/home/data/docker-containers/plex
+PLEX_HOME_DIR=$DOCKER_DATA/plex
 
 # --- 1. Define Variables ---
 
@@ -60,7 +62,7 @@ PLEX_MOVIES="$PLEX_HOME_DIR/library/movies"
 PLEX_TV="$PLEX_HOME_DIR/library/tv-shows"
 PLEX_CLAIM="YOUR_PLEX_CLAIM_TOKEN"
 TIME_ZONE="Europe/Bucharest"
-mkdir -p /home/docker-compose/plex
+mkdir -p $DOCKER_COMPOSE_DIR/plex
 
 # --- 2. Create the Directory Structure (Best Practice) ---
 mkdir -p "$PLEX_CONFIG"
@@ -70,7 +72,7 @@ mkdir -p "$PLEX_TV"
 # --- 3. Generate the docker-compose.yml File using a Here Document (EOF) ---
 
 # The variables within the EOF block will be expanded by the shell.
-cat << EOF > /home/docker-compose/plex/docker-compose.yml
+cat << EOF > $DOCKER_COMPOSE_DIR/plex/docker-compose.yml
 version: "2.1"
 services:
   plex:
@@ -99,7 +101,7 @@ echo ""
 echo "Run 'docker compose up -d' to start Plex."
 
 # Change to docker compose directory and start the container
-cd /home/docker-compose/plex/
+cd $DOCKER_COMPOSE_DIR/plex/
 docker compose up -d
 
 
@@ -115,16 +117,21 @@ EOF
 
 # Configure HA with standard configuration
 
-HA_CONF_DIR=/home/data/docker-containers/homeassistant/config
+HA_CONF_DIR=$DOCKER_DATA/homeassistant/config
+HA_MEDIA_DIR=$DOCKER_DATA/homeassistant/media
 mkdir -p $HA_CONF_DIR
-mkdir -p /home/docker-compose/homeassistant
-cat << EOF > /home/docker-compose/homeassistant/docker-compose.yml
+mkdir -p $DOCKER_COMPOSE_DIR/homeassistant
+cat << EOF > $DOCKER_COMPOSE_DIR/homeassistant/docker-compose.yml
 services:
   homeassistant:
     container_name: homeassistant
     image: "ghcr.io/home-assistant/home-assistant:stable"
+    dns: 
+      - 1.1.1.1 # Cloudflare DNS (Very fast and reliable)
+      - 8.8.8.8 # Google Public DNS (Secondary backup)
     volumes:
       - $HA_CONF_DIR:/config
+      - $HA_MEDIA_DIR:/media:ro
       - /etc/localtime:/etc/localtime:ro
       - /run/dbus:/run/dbus:ro
     devices:
@@ -134,21 +141,73 @@ services:
     network_mode: host
 EOF
 
-cd /home/docker-compose/homeassistant
+cd $DOCKER_COMPOSE_DIR/homeassistant
 docker compose up -d
 
 
 # Configure Paperless-ngx
+PAPERLESS_DIR=$DOCKER_DATA/paperless
+PAPERLESS_FOLDERS=("config" "data" "media" "export" "consume")
+for folder in "${PAPERLESS_FOLDERS[@]}"; do
+    mkdir -p "$PAPERLESS_DIR/$folder"
+    echo "Created: $PAPERLESS_DIR/$folder"
+done
 
+mkdir -p $DOCKER_COMPOSE_DIR/paperless
+cat << EOF > $DOCKER_COMPOSE_DIR/paperless/docker-compose.yml
+version: "3.4"
+services:
+  broker:
+    image: docker.io/library/redis:7
+    restart: unless-stopped
+    volumes:
+      - ./redis:/data:z  # :z fixes SELinux permissions on Rocky
 
+  db:
+    image: docker.io/library/postgres:16
+    restart: unless-stopped
+    volumes:
+      - ./db:/var/lib/postgresql/data:z
+    environment:
+      POSTGRES_DB: paperless
+      POSTGRES_USER: paperless
+      POSTGRES_PASSWORD: paperless_db_password # Change this for security!
+
+  webserver:
+    image: ghcr.io/paperless-ngx/paperless-ngx:latest
+    restart: unless-stopped
+    depends_on:
+      - db
+      - broker
+    ports:
+      - "8000:8000"
+    volumes:
+      - $PAPERLESS_DIR/data:/usr/src/paperless/data:z
+      - $PAPERLESS_DIR/media:/usr/src/paperless/media:z
+      - $PAPERLESS_DIR/export:/usr/src/paperless/export:z
+      - $PAPERLESS_DIR/consume:/usr/src/paperless/consume:z
+    environment:
+      PAPERLESS_REDIS: redis://broker:6379
+      PAPERLESS_DBHOST: db
+      PAPERLESS_DBPASS: paperless_db_password # Must match DB password above
+      PAPERLESS_SECRET_KEY: change-me-to-a-long-random-string
+      PAPERLESS_URL: http://localhost:8000
+      PAPERLESS_TIME_ZONE: Europe/Bucharest
+      PAPERLESS_OCR_LANGUAGE: eng # Change to 'ron' for Romanian OCR support
+      USERMAP_UID: 1000 # Your user ID (run `id` to check)
+      USERMAP_GID: 1000 # Your group ID
+EOF
+
+cd $DOCKER_COMPOSE_DIR/paperless/
+docker compose up -d
 
 ## Configure Mealie
 # We need to change the secret of the DB
-MEALIE_DATA=/home/data/docker-containers/mealie
+MEALIE_DATA=$DOCKER_DATA/mealie
 
-mkdir -p /home/docker-compose/mealie
+mkdir -p $DOCKER_COMPOSE_DIR/mealie
 mkdir -p $MEALIE_DATA
-cat << EOF > /home/docker-compose/mealie/docker-compose.yml
+cat << EOF > $DOCKER_COMPOSE_DIR/mealie/docker-compose.yml
 version: '3.8'
 
 services:
@@ -193,5 +252,21 @@ services:
       - POSTGRES_DB=mealie_db
 EOF
 
-cd /home/docker-compose/mealie
+cd $DOCKER_COMPOSE_DIR/mealie
+docker compose up -d
+
+METUBE_DATA=/home/data/metube/downloads
+mkdir -p $DOCKER_COMPOSE_DIR/metube/downloads
+cat << EOF > $DOCKER_COMPOSE_DIR/metube/docker-compose.yml
+services:
+  metube:
+    image: ghcr.io/alexta69/metube
+    container_name: metube
+    restart: unless-stopped
+    ports:
+      - "8081:8081" # Web UI access
+    volumes:
+      - $METUBE_DATA:/downloads # IMPORTANT: Persistent storage for files
+EOF
+cd $DOCKER_COMPOSE_DIR/metube/
 docker compose up -d
